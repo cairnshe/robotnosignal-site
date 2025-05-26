05505ef7ca853ecc3f6645edc98d45eb2b3b5891
cairnshe <cairnshe388@gmail.com>
2025-05-26 18:11:36 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbrc6FV024PCp9SDlJ_-Y5QpbxdqvTYJI",
+  authDomain: "robot-no-signal.firebaseapp.com",
+  projectId: "robot-no-signal",
+  storageBucket: "robot-no-signal.appspot.com",
+  messagingSenderId: "485194816357",
+  appId: "1:485194816357:web:fc33550bd299927266df8c",
+  measurementId: "G-QQRM0D4RB9"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
