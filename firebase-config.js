dc476599b0eff7f81ab0030100e57a6426ddeb3f
cairnshe <cairnshe388@gmail.com>
2025-05-28 18:08:17 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDbrc6FV024PCp9SDlJ_-Y5QpbxdqvTYJI",
@@ -11,3 +12,9 @@ const firebaseConfig = {
   appId: "1:485194816357:web:fc33550bd299927266df8c",
   measurementId: "G-QQRM0D4RB9"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
