7c9cabe084ee58592e896b0b7d720b56b1d4dce9
cairnshe <cairnshe388@gmail.com>
2025-06-01 16:08:23 -0400

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,7 @@
 console.log("✅ shop.html script started");
 
-import { db, auth } from './firebase-config.js';
+import { db, auth } from '../firebase-config.js';
+
 console.log("✅ Firebase modules imported", auth);
 
 import {
