ecdacb8646662abc3ec9002ecff91794971ab675
cairnshe <cairnshe388@gmail.com>
2025-06-04 14:25:13 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ import {
   setPersistence,
   browserSessionPersistence
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-import { firebaseConfig } from "../firebase-config-raw.js";
+import { firebaseConfig } from "./firebase-config-raw.js";
 
 console.log("🚀 Firebase Config:", firebaseConfig);
 
