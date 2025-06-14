b0bb26d80afd6e53b58ce803778d2a893ae54c93
cairnshe <cairnshe388@gmail.com>
2025-06-13 22:34:46 -0400

    Update admin-ship.js

--- js/admin-ship.js
+++ js/admin-ship.js
@@ -107,8 +107,8 @@ window.markAsShipped = async function (productId, productName, buyerEmail) {
     // Refresh page
     location.reload();
 
-  } catch (error) {
-    console.error("Failed to send email:", error);
+.catch((error) => {
+    console.error('Failed to send email:', error);
     alert("Order marked as shipped, but failed to send email.");
-  }
-};
+});
+
