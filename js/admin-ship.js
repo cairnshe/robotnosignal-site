d0b829bebf93a1e5cfac541fa471bd0fec89eea8
cairnshe <cairnshe388@gmail.com>
2025-06-14 12:18:45 -0400

    Update admin-ship.js

--- js/admin-ship.js
+++ js/admin-ship.js
@@ -11,8 +11,6 @@ import {
   doc
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-// ✅ 加上这句以启用 EmailJS
-import emailjs from "https://cdn.jsdelivr.net/npm/emailjs-com@3.2.0/dist/email.min.js";
 
 // EmailJS config
 const EMAILJS_SERVICE_ID = 'service_45v1ooq';
